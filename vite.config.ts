8a9bbb57d982170d78811725dde62a19ee9f9394
dhanish-raja <dhanishthecoder9398@gmail.com>
2025-07-08 20:56:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/walmart-hack-project-home-page/", // 👈 required for GitHub Pages
   server: {
     host: "::",
     port: 8080,
